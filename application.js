73bead7ec7a427c5007384b9fe8c885c5fdcd9e0
Alan <ahgv1195@gmail.com>
2017-11-16 10:47:42 -0600

    port changes for heroku

--- application.js
+++ application.js
@@ -1,8 +1,7 @@
 var express = require('express');
        i18n = require('i18n');
 
-const hostname = '127.0.0.1';
-          port = 5000;
+const PORT = process.env.PORT || 5000
 
 i18n.configure({
   locales:['es', 'en'],
@@ -82,7 +81,4 @@ app.get('/post', function(req, res) {
   res.render('post');
 });
 
-
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
